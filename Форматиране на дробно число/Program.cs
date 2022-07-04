7986b29250d04413d08d211e28f7b402e3661dc9
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-07-04 15:49:53 +0300

    Add files via upload

---
+++ Форматиране на дробно число/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Форматиране_на_дробно_число
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            int m;
+            
+
+            Console.Write("Въведете дробно число: ");
+            n = double.Parse(Console.ReadLine());
+
+            Console.Write("Въведете броя на знаци след запетаята: ");
+            m = int.Parse(Console.ReadLine());
+
+            n = Math.Round(n, m);
+          
+
+            Console.WriteLine(n);
+
+            Console.ReadLine();
+        }
+    }
+}
